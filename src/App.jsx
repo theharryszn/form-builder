f1f95843ec8083b834a18954b95fbf5c47454ce1
Abidemi Ope <opeabidemi@gmail.com>
2022-10-04 22:26:30 +0100

    designed CreateAccount page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import CreateAccount from './views/CreateAccount';
 import Login from './views/Login';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>} />
+      <Route path='/create-account' element={<CreateAccount/>} />
    </Routes>
   );
 }
--- src/views/CreateAccount.jsx
+++ src/views/CreateAccount.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+const CreateAccount = () => {
   return (
     <div className='grid grid-cols-2 w-screen h-screen'>
       <div className='bg-indigo-600 col-span-1'>
@@ -9,8 +9,11 @@ const Login = () => {
       <div className='col-span-1 flex items-center justify-center'>
         <form className='flex flex-col items-center space-y-6'>
           <div className='flex flex-col items-center justify-center space-y-4'>
-            <h1 className='text-3xl font-bold smm:text-4xl'>Hello again!</h1>
-            <p>Login now to access you forms.</p>
+            <h1 className='text-3xl font-bold smm:text-4xl'>Welcome to Form Builder!</h1>
+            <p>The fastest way to build your online forms. Let's get you started</p>
+          </div>
+          <div className='flex flex-col space-y-2'>
+            <input type="text" placeholder='Your name' name='fullname' id='fullname' className='p-4 bg-indigo-50 rounded-md min-w-[24rem] outline-indigo-400'/>
           </div>
           <div className='flex flex-col space-y-2'>
             <input type="password" placeholder='Email Address' name='email' id='email' className='p-4 bg-indigo-50 rounded-md min-w-[24rem] outline-indigo-400'/>
@@ -19,11 +22,11 @@ const Login = () => {
             <input type="password" placeholder='Password' name='email' id='email' className='p-4 bg-indigo-50 rounded-md min-w-[24rem] outline-indigo-400'/>
           </div>
           <div className='flex flex-col space-y-2'>
-            <button className='p-4 bg-indigo-600 text-white font-medium rounded-md min-w-[24rem] outline-indigo-400'>Login</button>
+            <button className='p-4 bg-indigo-600 text-white font-medium rounded-md min-w-[24rem]'>Create Account</button>
           </div>
           <div className='flex flex-col space-y-2 items-center'>
-            <p>Don't have an account?</p>
-            <Link to="/create-account" className='text-indigo-600 font-medium'>Create an account</Link>
+            <p>Already have an account?</p>
+            <Link to="/" className='text-indigo-600 font-medium'>Log-in instead</Link>
           </div>
         </form>
       </div>
@@ -31,4 +34,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default CreateAccount
